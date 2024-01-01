8d8805e90b508ddf0d14c2e6bb8787ba1dfc15a5
Gyubin Han <yangglei2@naver.com>
2024-01-01 23:11:38 +0900

    init commit
    init commit

---
+++ VisionArt/src/main/java/com/mc/full17th2/VisionArtApplication.java
@@ -0,0 +1,13 @@
+package com.mc.full17th2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionArtApplication.class, args);
+	}
+
+}
